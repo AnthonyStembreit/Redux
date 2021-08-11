514c94e3e71038f9838fe43be784a2772775298c
AnthonyStembreit <stembreit@gmail.com>
2021-08-11 12:53:37 -0500

    ran create react app, removed src files, created actions reducers, and store

---
+++ client/src/actions.js
@@ -0,0 +1,4 @@
+export const UPDATE_NOTE = "UPDATE_NOTE";
+export const ADD_TO_NOTES = "ADD_TO_NOTES";
+export const REMOVE_FROM_NOTES = "REMOVE_FROM_NOTES";
+export const TOGGLE_NOTE = "TOGGLE_NOTE";
---
+++ client/src/reducers.js
@@ -0,0 +1,57 @@
+import {
+    UPDATE_NOTE,
+    ADD_TO_NOTES,
+    REMOVE_FROM_NOTES,
+    // TOGGLE_NOTE
+  } from "./actions";
+  
+  const initialState = {
+    notes: [],
+    noteOpen: false
+  };
+
+export const reducers = (state = initialState, action) => {
+    switch (action.type) {
+      case UPDATE_NOTE:
+        let newState = state.notes.map(note => {
+            if(note._id === action._id){
+                return action
+            }
+            else{
+                return note
+            }
+          });
+        return {
+          ...state,
+          notes: newState,
+        };
+
+    // case TOGGLE_NOTE:
+    //   return {
+    //     ...state,
+    //     cartOpen: !state.cartOpen
+    //   };
+  
+      case ADD_TO_NOTES:
+        return {
+          ...state,
+          notes: [...state.cart, action.note],
+        };
+
+  
+      case REMOVE_FROM_NOTES:
+        let newState = state.notes.filter(note => {
+          return note._id !== action._id;
+        });
+  
+        return {
+          ...state,
+          notes: newState
+        };
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default reducers;
