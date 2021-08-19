406627edf9da140e7e7c6a1dd8146fc32b12e7ef
AnthonyStembreit <stembreit@gmail.com>
2021-08-19 14:22:04 -0500

    connected navbar, now working

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router,  Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import { Provider } from 'react-redux';
 import store from './utils/store';
 
 import AddNote from './pages/AddNote';
-import ViewNotes from './pages/View-Notes'
+import ViewNotes from './pages/View-Notes';
+import NavBar from './components/Nav-Bar';
 
 export default function App() {
 
     return (
         <Router>
-            <header><h1>Redux Counter</h1></header>
+            <header>
+                <h1>Redux Counter</h1>
+                <NavBar/>
+            </header>
             <div>
                 <Provider store={store}>
                     <Switch>
                         <Route exact path="/" component={AddNote} />
-                        <Route exact path="/all-notes" component={ViewNotes} />
-                        <Route exact path="/add-notes" component={AddNote} />
+                        <Route path="/view-notes" component={ViewNotes} />
+                        <Route path="/add-notes" component={AddNote} />
                     </Switch>
                 </Provider>
             </div>
--- client/src/components/Nav-Bar.js
+++ client/src/components/Nav-Bar.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 export default function NavBar(){
     return (
         <navbar>
-            <Link to="/view-notes">View All</Link>
-            <Link to="add-note">Add New</Link>
+            <Link to="/add-notes">Create</Link>
+            <Link to="/view-notes">View</Link>
         </navbar>
     )
 }
